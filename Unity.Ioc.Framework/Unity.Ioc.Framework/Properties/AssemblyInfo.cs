655615294028c1742f461e9c27492d6ba77399ba
luohongtao <luohongtao621@hotmail.com>
2026-04-11 13:39:02 +0800

    Upgrade to .NET Framework 4.8

--- Unity.Ioc.Framework/Unity.Ioc.Framework/Properties/AssemblyInfo.cs
+++ Unity.Ioc.Framework/Unity.Ioc.Framework/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("Unity.Ioc.Framework")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("a unity IoC framework extension for Microsoft.Practices.Unity")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("meloht")]
 [assembly: AssemblyProduct("Unity.Ioc.Framework")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
